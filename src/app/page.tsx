3b87901c2115ddcab08a08c30609ad916788f7c2
Kostas <diamantopoulos.kostas@hotmail.com>
2024-06-30 12:32:51 +0100

    added express js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,20 +76,15 @@ export default function Home1() {
       <div className="flex flex-col w-1/4 bg-indigo-950 divide-y divide-double ">
         {/* Navigation */}
         <nav className="flex-1 overflow-y-auto">
-           <Image
-              className="h-14 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
-           {/* <svg viewBox="0 0 926 676" aria-hidden="true" className="absolute left-24 w-[57.875rem] transform-gpu blur-[118px]">
-        <path fill="url(#60c3c621-93e0-4a09-a0e6-4c228a0116d8)" fill-opacity=".4" d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z" />
-        <defs>
-          <linearGradient id="60c3c621-93e0-4a09-a0e6-4c228a0116d8" x1="926.392" x2="-109.635" y1=".176" y2="321.024" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#776FFF" />
-            <stop offset="1" stop-color="#FF4694" />
-          </linearGradient>
-        </defs>
-      </svg>*/}
+
+          <Image
+            className="h-14 w-14 mt-10 ml-5"
+            src="/terrier.png"
+            alt="Your Company"
+            width={100}
+            height={100}
+          />
+          
           <ul className="flex flex-col gap-y-5 mt-20">
             {navigation.map((item) => (
               <li key={item.name}>
@@ -141,8 +136,10 @@ export default function Home1() {
                     className="flex mb-12 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
                   >
                     <Image
-                      className="h-10 w-10 rounded-full bg-gray-800"
-                      src="avatar.jpeg"
+                      className=" rounded-full bg-gray-800"
+                      src="/avatar.jpeg"
+                      width={70}
+                      height={70}
                       alt=""
                     />
                     <span className="sr-only">Your profile</span>
